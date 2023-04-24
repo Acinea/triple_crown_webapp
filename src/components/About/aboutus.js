0b0ad08932b696e61d74db17f68cfcbe1f389427
James Kennedy <acinea.kennedy@gmail.com>
2023-04-24 16:52:45 -0400

    Set up About section

---
+++ src/components/About/aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './about.css'
+
+export function About() {
+    return (
+        <main className='about-container'>
+            <h3>About Us</h3>
+            <p>At Triple Crown , we are passionate about bringing sports fans closer to their favorite athletes. We believe that there is no better way to connect with your sports heroes than to meet them in person and obtain an autograph. Our events provide a unique opportunity for fans to interact with some of the biggest names in the sports industry, including baseball, football, basketball, and more.</p>
+        </main>
+    )
+}
