90b817ae27618f9c42f2fcbe21b1a8f60500c77a
James Kennedy <acinea.kennedy@gmail.com>
2023-04-24 16:10:07 -0400

    Created contact form & styles

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './contact.css'
+
+export function Contact() {
+    return (
+        <main className='contact-container'>
+            <section>
+                <h3>Triple Crown Promotions</h3>
+                <p>With a passion for sports and a commitment to providing unnforgettable experiences for fans, Triple Crown Promotions is quickly becoming renowned for its highly-anticipated events. For more information on upcoming events, or to inquire about potiential partnerships, please complete the form below, and one of our event specialists will get back to you shortly!</p>
+            </section>
+            <section className='contact-form-container'>
+                <form className='contact-form'>
+                    <label>Name</label>
+                    <input></input>
+                    <label>Email</label>
+                    <input></input>
+                    <label>Message</label>
+                    <textarea></textarea>
+                    <div className='submit-container'>
+                        <button id="submit-button">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    )
+}
