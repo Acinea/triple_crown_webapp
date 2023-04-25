9ad80380d25a9ff13ec0a76b5971710917c26253
James Kennedy <acinea.kennedy@gmail.com>
2023-04-25 08:27:24 -0400

    Hiding api Key

--- src/components/Location/location.js
+++ src/components/Location/location.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {url} from '../../resources/local'
 import './location.css'
 
 export function Location() {
     return (
         <main>
-            <img id="location-img" width="300" src="https://maps.googleapis.com/maps/api/staticmap?center=Knights+of+Columbus+61+pine+street%2C+Old+Bridge%2C+NJ&zoom=14&scale=2&size=300x500&maptype=roadmap&format=png&key=AIzaSyDaqeG5Ji7o7w4wkL6T7M7xmgibWIemxDQ&markers=size:mid%7Ccolor:0xf9061f%7Clabel:%7CKnights%20of%20Columbus%2061%20pine%20street%2C%20Old%20Bridge%2C%20NJ" alt="Google map of Knights of Columbus 61 pine street, Old Bridge, NJ" />
+            <img id="location-img" width="300" src={url} alt="Google map of Knights of Columbus 61 pine street, Old Bridge, NJ" />
         </main>
     )
 }
