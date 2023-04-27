b7af51cf5802d0141bb1405e2ed43dc9fc4cf4dc
James Kennedy <acinea.kennedy@gmail.com>
2023-04-27 15:38:25 -0400

    Updated time

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -35,7 +35,7 @@ export function Home() {
             <section>
                 <article className='presents'>
                     <h2 className='presents-title'>Triple Crown Promotion Presents: Dwight Gooden</h2>
-                    <h3 className='presents-title'>June 11th, 1pm - 4pm</h3>
+                    <h3 className='presents-title'>June 11th, 1pm - 5pm</h3>
                     <h3 className='presents-title'>Old Bridge Knights of Columbus</h3>
                     <img id="player-image" src={player} alt='Dwight Gooden' />
                     <p>Join us at <b>The Old Bridge Knights of Columbus</b>, Sunday June 11th for an afternoon of sports autography featuring none other than the legendary Dwight Gooden. As one of the most iconic figures in baseball history, Gooden's impact on the sport is immeasurable. Fans from all over are gathering to this event to witness his incredible talent and have the opportunity to get their memoriabilia signed by the man himself.</p>
