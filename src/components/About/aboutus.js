0789e36d5c02a91ef62c88d01f789ba1798121ad
James Kennedy <acinea.kennedy@gmail.com>
2023-04-27 15:37:50 -0400

    Fixed typo

--- src/components/About/aboutus.js
+++ src/components/About/aboutus.js
@@ -5,7 +5,7 @@ export function About() {
     return (
         <main id="about" className='about-container'>
             <h3>About Us</h3>
-            <p>At Triple Crown , we are passionate about bringing sports fans closer to their favorite athletes. We believe that there is no better way to connect with your sports heroes than to meet them in person and obtain an autograph. Our events provide a unique opportunity for fans to interact with some of the biggest names in the sports industry, including baseball, football, basketball, and more.</p>
+            <p>At Triple Crown Promotion, we are passionate about bringing sports fans closer to their favorite athletes. We believe that there is no better way to connect with your sports heroes than to meet them in person and obtain an autograph. Our events provide a unique opportunity for fans to interact with some of the biggest names in the sports industry, including baseball, football, basketball, and more.</p>
         </main>
     )
 }
