e2d4e899ef66c7f342e4ac25ecdf2de58fb42cab
James Kennedy <acinea.kennedy@gmail.com>
2023-04-27 14:20:58 -0400

    Editing schedule styles

--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import logo from '../../resources/tcp logo.jpg'
 import './schedule.css'
 
 
 export function Schedule(props) {
     return (
         <section className={props.showSchedule ? 'schedule-container' : 'hide-schedule-container'}>
-            <div className='close-schedule-button' onClick={props.toggleSchedule}><CloseIcon/></div>
-            <h1 className='schedule-title'>Event Schedule</h1>
-            <h3><b>Schedule</b></h3>
-            <p>Inscription: $12</p>
-            <p>Autograph: $30</p>
-            <p>Photo Op: $25</p>
-            <p>Combo {`(Autograph & Photo)`}: $50</p>
-            <h3>Tables</h3>
-            <p>6' Table: $50 or 2 for $90</p>
-            <p>8' Table: $60 or 2 for $100</p>
-            
+            <div>
+                <div className='close-schedule-button' onClick={props.toggleSchedule}><CloseIcon/></div>
+                <h1 className='schedule-title'>Event Schedule</h1>
+            </div>
+            <div>
+                <h2>1:00pm</h2><p>Doors Open</p>
+                <h2>2:00pm - 2:30pm</h2><p>Photo Opportunities</p>
+                <h2>2:30pm - 4:00pm</h2><p>Memoriabilia Signing</p>
+                <h2>5:00pm</h2><p>Event Ends</p>
+            </div>
+            <img  className='schedule-logo' src={logo} alt='company logo' />
         </section>
     )
 }
