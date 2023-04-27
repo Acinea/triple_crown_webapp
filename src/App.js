cbc2c228a92bad6d38cd8ef04a27c2526bbfc89f
James Kennedy <acinea.kennedy@gmail.com>
2023-04-27 13:10:08 -0400

    Added Price / Schedule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import {Header} from './components/Header/header'
 import {Home} from './components/Home/home'
+import {Price} from './components/Price/price'
 import {Location} from './components/Location/location'
 import {Contact} from './components/Contact/contact'
 import {About} from './components/About/aboutus'
