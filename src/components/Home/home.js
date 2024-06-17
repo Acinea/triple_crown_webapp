efefea1b2417b44b05355797a50be8d1f857ae3f
Acinea <acinea.kennedy@gmail.com>
2024-06-17 16:57:13 -0400

    Added WildBoys Event, readded purchase button

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -30,18 +30,18 @@ export function Home() {
                     <img id="banner-logo" src={logo} alt='company-logo' />
                     <p id='banner-info'>Triple Crown Promotion is a premier sports event organizing company in New Jersey which specializes in bringing sports fans and athletes together through autographing events.</p>
                     <div className="banner-button-container">
+                        <button id="banner-button" onClick={() => window.open('https://www.ezevent.com/Event/sports-card-collectibles-show-tickets-7622', '_blank')}>Purchase Tickets</button>
                         <button id="banner-button"  onClick={() => scrollSmoothTo('contact')}>Contact</button>
-                        
                     </div>
                 </article>
             </section>
             <section>
                 <article className='presents'>
                     <h2 className='presents-title'>Triple Crown Promotion Presents:<br></br> Sportscards & Collectibles Show</h2>
-                    <h4 className='presents-title-info'>Saturday, May 4th, 9am - 2pm</h4>
+                    <h4 className='presents-title-info'>Saturday, June 22nd, 9am - 2pm</h4>
                     <h4 className='presents-title-info'>Old Bridge First Aid & Rescue Squad</h4>
                     <img className='presents-image' src={card} alt='image of trading cards'></img>
-                    <p className='presents-info'>Join us at <b>Old Bridge First Aid & Rescue Squad</b>, Saturday, May 4th as we showcase a trove of autographed cards and Collectibles. Relive the moments share stories, and connect with fellow enthusiasts who love the game just as much as you do.</p>
+                    <p className='presents-info'>Join us at <b>Old Bridge First Aid & Rescue Squad</b>, Saturday, June 22nd as we showcase a trove of autographed cards and Collectibles. Relive the moments share stories, and connect with fellow enthusiasts who love the game just as much as you do.</p>
                     <p className='presents-info'><b>Admission Price: </b>$3</p>
                     <p className='presents-info'><b>Location:</b> 200 Marlboro Road, Old Bridge, NJ 08857</p>
                 </article>
--- src/components/Upcoming/upcoming.js
+++ src/components/Upcoming/upcoming.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import card from '../../resources/sportcard.jpg'
+import wild from '../../resources/wildboys.jpg'
 import './upcoming.css'
 
 export function Upcoming() {
@@ -8,10 +9,29 @@ export function Upcoming() {
             <h1 id="upcoming-container-title">Upcoming Events</h1>
             <section className='upcoming-event-container'>
                 <div className='upcoming-event-title'>
-                    <h2 className='upcoming-event-title-info'>Saturday, June 22nd</h2>
+                    <h2 className='upcoming-event-title-info'>Saturday, July 13th</h2>
                     <h2 className='upcoming-event-title-info'>Triple Crown Promotion Presents:<br></br> Sportscards & Collectibles Show</h2>
                     <h2 className='upcoming-event-title-info'>Old Bridge First Aid & Rescue</h2>
                     <img className='upcoming-image' src={card} alt="sports cards" />
+                    {/*<h2>More Events Coming Soon.</h2> */}
+                </div>
+            </section>
+            <section className='upcoming-event-container'>
+                <div className='upcoming-event-title'>
+                    <h2 className='upcoming-event-title-info'>Saturday, August 3rd</h2>
+                    <h2 className='upcoming-event-title-info'>Triple Crown Promotion Presents:<br></br> Sportscards & Collectibles Show</h2>
+                    <h2 className='upcoming-event-title-info'>Old Bridge First Aid & Rescue</h2>
+                    <img className='upcoming-image' src={card} alt="sports cards" />
+                    {/*<h2>More Events Coming Soon.</h2> */}
+                </div>
+            </section>
+            <section className='upcoming-event-container'>
+                <div className='upcoming-event-title'>
+                    <h2 className='upcoming-event-title-info'>Saturday, September 7th</h2>
+                    <h2 className='upcoming-event-title-info'>Triple Crown Promotion Presents:<br></br> The Wild Boys: Wally Backman & Lenny Dykstra</h2>
+                    <h2 className='upcoming-event-title-info'>Old Bridge First Aid & Rescue</h2>
+                    <img className='upcoming-image' src={wild} alt="sports cards" />
+                    <button id="banner-button" onClick={() => window.open('https://www.ezevent.com/Event/sports-card-collectibles-show-tickets-7622', '_blank')}>Purchase Tickets</button>
                     {/*<h2>More Events Coming Soon.</h2> */}
                 </div>
             </section>
